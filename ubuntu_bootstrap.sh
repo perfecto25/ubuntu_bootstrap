c3835b4b3a1de0614e0ca430324c13c78c8e5c3f
mrx <mike.reider@gmail.com>
2023-08-10 14:46:19 -0400

    Update ubuntu_bootstrap.sh

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -110,6 +110,12 @@ sudo update-grub
 cp files/.bashrc ~
 cp files/.zshrc ~
 
+## AnyDesk
+sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update
+sudo apt install anydesk
+
 
 ###################################
 # battery and power
